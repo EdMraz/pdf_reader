784336b0175eb68dd3e6958da6b83327c6b37d38
EdMraz <eduard.mraz05@gmail.com>
2023-04-04 21:45:58 +0200

    initial commit

---
+++ pdf_reader.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from PyPDF2 import PdfReader
+from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root,width=600,height=600)
+canvas.grid(columnspan=3,rowspan=3)
+
+#logo
+logo = Image.open('logo50.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1,row=0)
+
+#instructions
+instructions = tk.Label(root, text="Select a PDF file on your PC to extract all its text",font="Ariel")
+instructions.grid(columnspan=3,column=0,row=1)
+
+def open_file():
+    browse_text.set("loading...")
+    file = askopenfile(parent=root,mode="rb",title="Choose a file", filetype=[("Pdf file","*.pdf")])
+    if file:
+        reader = PdfReader(file)
+        text = ""
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
+
+        #text box
+        text_box = tk.Text(root,height=10,width=50,padx=15,pady=15)
+        text_box.insert(1.0,text)
+        text_box.tag_configure("center",justify="center")
+        text_box.tag_add("center",1.0,"end")
+        text_box.grid(column=1,row=3)
+
+        browse_text.set("Browse")
+
+#browse button
+browse_text = tk.StringVar()
+browse_btn = tk.Button(root,textvariable=browse_text, command=lambda:open_file(),font="Ariel",bg="white",fg="black",height=2, width=15)
+browse_text.set("Browse")
+browse_btn.grid(column=1,row=2)
+
+canvas = tk.Canvas(root, width=600,height=250)
+canvas.grid(columnspan=3)
+
+root.mainloop()
